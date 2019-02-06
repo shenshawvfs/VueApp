9a4c2679cb95d6758caf1008ff0b448bfe2aba18
Scott Henshaw <shenshaw@vfs.com>
2019-02-05 17:38:31 -0800

    Working on it.  best way to do components?

--- app/components/sample.component.js
+++ app/components/sample.component.js
@@ -10,17 +10,7 @@ Copyright (c) 2019. Scott Henshaw, Kibble Online Inc. All Rights Reserved.
 */
 'use strict';
 
-class VUEController {
-
-    set style( css = "") {
-        
-        let theStyles = document.createTextNode( css );
-        let componentCSS = document.createElement('style');
-        componentCSS.appendChild( theStyles );
-        
-        document.head.appendChild( componentCSS );
-    }
-}
+import VUEController from '../lib/VUEController.js';
 
 class SampleController extends VUEController {
 
@@ -38,63 +28,12 @@ class SampleController extends VUEController {
     //--------------------------------------------------------
     doSomething() {
         // trigger this inside the template...
-    }
-}
+        let debug = true;
 
-class ComponentDefinition {
-
-    constructor( name = "componentDefinition") {
-        this.name = "";
-        this.vm = {};
-        this.props = {};
-        this.methods = {};
-        this.template = "";
     }
 
-    model( dataObj  ) {
-        Object.apply( this.vm, dataObj );
-        return this
-    }
-
-    attributes( properties ) { 
-        Object.apply( this.props, properties );
-        return this
-    }
     
-    controller( proto ) {
-        Object.apply( this.methods, proto );
-        return this
-    }
-
-    markup( template ) {
-        this.template = template;
-        return this
-    }
-
-    data() {
-        return this.vm;
-    }
 }
-
-
-let defn = new ComponentDefinition('sampleComponent')
-    .model({ 
-        name: "Scott" 
-    })
-    .attributes({
-        widget:    String,
-        enum:      Number,
-        available: Boolean,
-        author:    Object
-    })
-    .controller( new SampleController() )
-    .markup(`
-    <label class="sample-bar">
-        {{ name }}
-        <input v-bind:value="widget" v-on:input="$emit('input', $event.target.value )">
-        <slot></slot>
-    </label>`);
-
 
 export default Vue.component('sample-component', {
         name: 'sample-component',
